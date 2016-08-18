f12166abd7d4a5fe0bb3e07fe193aade4b980128
Andreas Zöllner <andi@go-for-fun.de>
2016-08-18 17:52:54 +0300

    clone NSView

---
+++ NSView+clone.h
@@ -0,0 +1,15 @@
+//
+//  NSView+clone.h
+//  exchangeExport
+//
+//  Created by Andreas Zöllner on 17.08.16.
+//  Copyright (c) 2016 Studio Istanbul. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSView (clone)
+
+-(id)clone;
+
+@end
